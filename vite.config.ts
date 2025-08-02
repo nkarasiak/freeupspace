f75a403d0950ccfdce9571193b98b16a53fce108
Nicolas Karasiak <nicart@gmail.com>
2025-08-02 21:27:16 +0200

    use vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,49 @@
 import { defineConfig } from 'vite'
+import checker from 'vite-plugin-checker'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/freeupspace/' : '/',
   publicDir: 'public',
+  plugins: [
+    checker({
+      typescript: true,
+    }),
+    visualizer({
+      filename: 'dist/stats.html',
+      open: false,
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
   server: {
     port: 3000,
+    host: true,
+    open: true,
+    hmr: {
+      overlay: true,
+    },
   },
   build: {
     outDir: 'dist',
     sourcemap: true,
+    target: 'esnext',
+    minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'deck-gl': ['@deck.gl/core', '@deck.gl/layers', '@deck.gl/geo-layers', '@deck.gl/aggregation-layers', '@deck.gl/mapbox'],
+          'maplibre': ['maplibre-gl'],
+          'satellite': ['satellite.js'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+  },
+  optimizeDeps: {
+    include: ['@deck.gl/core', '@deck.gl/layers', 'maplibre-gl', 'satellite.js'],
+  },
+  define: {
+    __DEV__: JSON.stringify(process.env.NODE_ENV === 'development'),
   },
 })
